a61c0af1f5d58104c39168269b6e19ab5e6f2f66
josur <josur@10.164.52.143>
2022-11-11 11:59:10 +0100

    VInicio2

--- StravaClient/src/es/deusto/ingenieria/sd/auctions/client/VentanaInicio.java
+++ StravaClient/src/es/deusto/ingenieria/sd/auctions/client/VentanaInicio.java
@@ -6,6 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaInicio extends JFrame {
 
@@ -37,9 +46,24 @@ public class VentanaInicio extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JButton btnNewButton = new JButton("Registrar");
+		btnNewButton.setBounds(156, 140, 116, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("STRAVA");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setBounds(156, 11, 116, 78);
 		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton_1 = new JButton("Iniciar sesión");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(156, 189, 116, 23);
+		contentPane.add(btnNewButton_1);
 	}
 
 }
