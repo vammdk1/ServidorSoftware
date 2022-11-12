82480c4812bd3811f7d43b9e7f89eb8af846eda9
MarianAngulo <81021160+MarianAngulo@users.noreply.github.com>
2022-11-12 18:20:45 +0100

    Ventanas registro y login sin implementacion

--- StravaClient/src/es/deusto/ingenieria/sd/auctions/client/VentanaRegistro.java
+++ StravaClient/src/es/deusto/ingenieria/sd/auctions/client/VentanaRegistro.java
@@ -22,7 +22,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
 
-public class VentanaLogin {
+public class VentanaRegistro {
 	static JFrame VPrincipal = new JFrame("STRAVA");
 	static int x = 500;
 	static int y = 300;
@@ -46,19 +46,15 @@ public class VentanaLogin {
 		panelInferior.add(PBotones);
 		JPanel PBotones1 = new JPanel();
 		JButton BRegistro = new JButton();
-		BRegistro.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		BRegistro.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		BRegistro.setText("Registrarme");
-		JButton BInicioSecion = new JButton();
-		BInicioSecion.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BInicioSecion.setText("Iniciar Sesion");
 		JPanel PBotones2 = new JPanel();
 		JButton BGoogle = new JButton();
-		BGoogle.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BGoogle.setText("Iniciar con Google");
+		BGoogle.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		BGoogle.setText("Registrar con Google");
 		JButton BFacebook = new JButton();
-		BFacebook.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BFacebook.setText("Iniciar con Facebook");
-		PBotones1.add(BInicioSecion);
+		BFacebook.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		BFacebook.setText("Registrar con Facebook");
 		PBotones1.add(BRegistro);
 		PBotones2.add(BGoogle);
 		PBotones2.add(BFacebook);
@@ -69,21 +65,41 @@ public class VentanaLogin {
 		VPrincipal.getContentPane().add(PanelGlobal,BorderLayout.CENTER);
 		JTextArea Correo = new JTextArea();
 		Correo.setFont(new Font("Monospaced", Font.PLAIN, 20));
-		Correo.setBounds(37, 80, 663, 43);
+		Correo.setBounds(37, 179, 663, 31);
 		PanelGlobal.add(Correo);
 		JPasswordField Contrasenna = new JPasswordField();
 		Contrasenna.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		Contrasenna.setBounds(37, 195, 663, 55);
+		Contrasenna.setBounds(37, 240, 663, 31);
 		PanelGlobal.add(Contrasenna);
 		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNombre.setBounds(37, 11, 289, 38);
+		PanelGlobal.add(lblNombre);
+		
+		JTextArea nombre = new JTextArea();
+		nombre.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		nombre.setBounds(37, 122, 663, 31);
+		PanelGlobal.add(nombre);
+		
+		JLabel lblFechaDeNacimiento = new JLabel("Fecha de nacimiento");
+		lblFechaDeNacimiento.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblFechaDeNacimiento.setBounds(37, 83, 289, 37);
+		PanelGlobal.add(lblFechaDeNacimiento);
+		
+		JTextArea fecha = new JTextArea();
+		fecha.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		fecha.setBounds(37, 53, 663, 31);
+		PanelGlobal.add(fecha);
+		
 		JLabel lblCorreo = new JLabel("Correo electonico");
-		lblCorreo.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblCorreo.setBounds(37, 24, 289, 50);
+		lblCorreo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblCorreo.setBounds(37, 150, 289, 31);
 		PanelGlobal.add(lblCorreo);
 		
 		JLabel lblContrasena = new JLabel("Contrasena");
-		lblContrasena.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblContrasena.setBounds(37, 134, 163, 50);
+		lblContrasena.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblContrasena.setBounds(37, 208, 163, 31);
 		PanelGlobal.add(lblContrasena);
 		
 		JPanel panelSuperior = new JPanel();
--- StravaClient/src/es/deusto/ingenieria/sd/auctions/client/VentanaRegistro2.java
+++ StravaClient/src/es/deusto/ingenieria/sd/auctions/client/VentanaRegistro2.java
@@ -22,7 +22,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
 
-public class VentanaLogin {
+public class VentanaRegistro2 {
 	static JFrame VPrincipal = new JFrame("STRAVA");
 	static int x = 500;
 	static int y = 300;
@@ -47,44 +47,57 @@ public class VentanaLogin {
 		JPanel PBotones1 = new JPanel();
 		JButton BRegistro = new JButton();
 		BRegistro.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BRegistro.setText("Registrarme");
-		JButton BInicioSecion = new JButton();
-		BInicioSecion.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BInicioSecion.setText("Iniciar Sesion");
-		JPanel PBotones2 = new JPanel();
-		JButton BGoogle = new JButton();
-		BGoogle.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BGoogle.setText("Iniciar con Google");
-		JButton BFacebook = new JButton();
-		BFacebook.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		BFacebook.setText("Iniciar con Facebook");
-		PBotones1.add(BInicioSecion);
+		BRegistro.setText("Continuar");
 		PBotones1.add(BRegistro);
-		PBotones2.add(BGoogle);
-		PBotones2.add(BFacebook);
 		PBotones.setLayout(new BorderLayout());
 		PBotones.add(PBotones1,BorderLayout.NORTH);
-		PBotones.add(PBotones2,BorderLayout.SOUTH);
 		PBotones.setSize(x, y);
 		VPrincipal.getContentPane().add(PanelGlobal,BorderLayout.CENTER);
-		JTextArea Correo = new JTextArea();
-		Correo.setFont(new Font("Monospaced", Font.PLAIN, 20));
-		Correo.setBounds(37, 80, 663, 43);
-		PanelGlobal.add(Correo);
-		JPasswordField Contrasenna = new JPasswordField();
-		Contrasenna.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		Contrasenna.setBounds(37, 195, 663, 55);
-		PanelGlobal.add(Contrasenna);
 		
-		JLabel lblCorreo = new JLabel("Correo electonico");
-		lblCorreo.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblCorreo.setBounds(37, 24, 289, 50);
-		PanelGlobal.add(lblCorreo);
+		JTextArea nombre = new JTextArea();
+		nombre.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		nombre.setBounds(120, 70, 500, 31);
+		PanelGlobal.add(nombre);
 		
-		JLabel lblContrasena = new JLabel("Contrasena");
-		lblContrasena.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblContrasena.setBounds(37, 134, 163, 50);
-		PanelGlobal.add(lblContrasena);
+		JLabel lblInfo = new JLabel("Esta informacion es opcional, si no quieres a\u00F1adirla pulsa en continuar.");
+		lblInfo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblInfo.setBounds(86, 0, 560, 25);
+		PanelGlobal.add(lblInfo);
+		
+		JLabel lblPeso = new JLabel("Peso(kg)");
+		lblPeso.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPeso.setBounds(115, 30, 289, 38);
+		PanelGlobal.add(lblPeso);
+		
+		JLabel lblAltura = new JLabel("Altura(cm)");
+		lblAltura.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblAltura.setBounds(115, 100, 163, 38);
+		PanelGlobal.add(lblAltura);
+		
+		JTextArea nombre_1 = new JTextArea();
+		nombre_1.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		nombre_1.setBounds(120, 140, 500, 31);
+		PanelGlobal.add(nombre_1);
+		
+		JLabel lblAltura_1 = new JLabel("Altura(cm)");
+		lblAltura_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblAltura_1.setBounds(115, 170, 163, 38);
+		PanelGlobal.add(lblAltura_1);
+		
+		JTextArea nombre_1_1 = new JTextArea();
+		nombre_1_1.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		nombre_1_1.setBounds(120, 210, 500, 31);
+		PanelGlobal.add(nombre_1_1);
+		
+		JLabel lblAltura_1_1 = new JLabel("Altura(cm)");
+		lblAltura_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblAltura_1_1.setBounds(115, 240, 163, 38);
+		PanelGlobal.add(lblAltura_1_1);
+		
+		JTextArea nombre_1_1_1 = new JTextArea();
+		nombre_1_1_1.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		nombre_1_1_1.setBounds(120, 280, 500, 31);
+		PanelGlobal.add(nombre_1_1_1);
 		
 		JPanel panelSuperior = new JPanel();
 		VPrincipal.getContentPane().add(panelSuperior, BorderLayout.NORTH);
