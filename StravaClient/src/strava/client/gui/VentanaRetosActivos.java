0abf4f9c8430cc5eee394937ff74103a48a08a80
yosoyjosu <josurevilla@opendeusto.es>
2022-11-13 17:28:45 +0100

    vRetosActivos

--- StravaClient/src/strava/client/gui/VentanaRetosActivos.java
+++ StravaClient/src/strava/client/gui/VentanaRetosActivos.java
@@ -47,8 +47,75 @@ public class VentanaRetosActivos {
 		JPanel Retos = new JPanel();
 		Retos.setBounds(29, 72, 684, 318);
 		VPrincipal.getContentPane().add(Retos);
+		Retos.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Retos activos");
+		JComboBox SelectorDeRetos = new JComboBox();
+		SelectorDeRetos.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		SelectorDeRetos.setBounds(286, 11, 239, 31);
+		Retos.add(SelectorDeRetos);
+		
+		JLabel lbl = new JLabel("Fecha Inicio");
+		lbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl.setBounds(98, 70, 136, 31);
+		Retos.add(lbl);
+		
+		JLabel FechaIni = new JLabel("00/00");
+		FechaIni.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		FechaIni.setBounds(318, 70, 277, 31);
+		Retos.add(FechaIni);
+		
+		JLabel lbl_1 = new JLabel("Fecha Fin");
+		lbl_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_1.setBounds(98, 123, 136, 31);
+		Retos.add(lbl_1);
+		
+		JLabel FechaFin = new JLabel("00/00");
+		FechaFin.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		FechaFin.setBounds(318, 123, 277, 31);
+		Retos.add(FechaFin);
+		
+		JLabel lbl_2 = new JLabel("Distancia Objetivo");
+		lbl_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_2.setBounds(26, 176, 208, 31);
+		Retos.add(lbl_2);
+		
+		JLabel DistObj = new JLabel("0");
+		DistObj.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		DistObj.setBounds(318, 176, 277, 31);
+		Retos.add(DistObj);
+		
+		JLabel lbl_3 = new JLabel("Tiempo Objetivo");
+		lbl_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_3.setBounds(50, 227, 184, 31);
+		Retos.add(lbl_3);
+		
+		JLabel TiObj = new JLabel("0");
+		TiObj.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		TiObj.setBounds(318, 227, 277, 31);
+		Retos.add(TiObj);
+		
+		JLabel lbl_4 = new JLabel("Deporte");
+		lbl_4.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_4.setBounds(98, 276, 136, 31);
+		Retos.add(lbl_4);
+		
+		JLabel Deporte = new JLabel("Null");
+		Deporte.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		Deporte.setBounds(318, 276, 277, 31);
+		Retos.add(Deporte);
+		
+		JLabel lblRetosActivos = new JLabel("Retos Activos");
+		lblRetosActivos.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblRetosActivos.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblRetosActivos.setBounds(119, 3, 136, 41);
+		Retos.add(lblRetosActivos);
+		
+		JLabel lblNewLabel = new JLabel("Aceptar Reto");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
 		lblNewLabel.setBounds(5, 5, 724, 49);
