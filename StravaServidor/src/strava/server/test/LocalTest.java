c956b484412d2f96b17c994cb6aa7b516963b6c4
Víctor <victor.martinez.dk@opendeusto.es>
2023-01-22 17:43:28 +0100

    arreglo errores al sacar de base de datos

--- StravaServidor/src/strava/server/test/LocalTest.java
+++ StravaServidor/src/strava/server/test/LocalTest.java
@@ -2,6 +2,13 @@ package strava.server.test;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import javax.jdo.Extent;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
 
 import strava.server.data.dao.RetoDAO;
 import strava.server.data.dao.SesionEntrenamientoDAO;
@@ -20,6 +27,8 @@ import strava.server.services.BaseDatos;
 
 public class LocalTest {
 	
+	private static PersistenceManagerFactory pmf;
+	
 	public static void main(String[] args) {	
 	RemoteFacade facade = null;
 	ArrayList<Reto> lr1 = null;
@@ -30,7 +39,10 @@ public class LocalTest {
 	long token2 = 02;
 	long token3 = 03;
 	
-	
+	pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+	PersistenceManager pm = pmf.getPersistenceManager();
+	Transaction tx = pm.currentTransaction();
+	/**
 	try {
 		facade = new RemoteFacade();
 		System.out.println("/////////////////////////////////////////////////////////////////////////");
@@ -47,7 +59,7 @@ public class LocalTest {
 		System.out.println(token);
 	} catch (Exception e) {
 		System.out.println("\t# Error: " + e.getMessage());	
-	}
+	}**/
 	/**
 	//ver datos en el servidor
 	try {
@@ -55,15 +67,81 @@ public class LocalTest {
 	} catch (Exception e) {
 		System.out.println("\t# Error: " + e.getMessage());	
 	}**/
+
 	User usuarioDAO = new UsuarioStrava("Alfonso", "alfonsoortega@gmail.com", "22/12/2001", 50, 175, 130, 100, Proveedor.LOCAL, "1234");
+	
+	tx.begin();
+	System.out.println("   * Storing an user: " + usuarioDAO);
+	pm.makePersistent(usuarioDAO);
+	tx.commit();
+	
 	Reto retoDAO = new Reto("Correr un poco", new Date(), new Date(), 10, 0, Deportes.RUNNING);
 	SesionEntrenamiento sesionEntrenamientoDAO = new SesionEntrenamiento("Cumbia mientras corro", Deportes.RUNNING, 10, new Date(), 60);
 	RetoDAO.getInstance().save(retoDAO);
 	SesionEntrenamientoDAO.getInstance().save(sesionEntrenamientoDAO);
+	
 	usuarioDAO.anadirReto(retoDAO);
 	usuarioDAO.anadirSesion(sesionEntrenamientoDAO);
 	System.out.println(usuarioDAO.getSesiones());
-	UserDAO.getInstance().save(usuarioDAO);
+	//-------------------------------------------------
+	
+	
+		pm = pmf.getPersistenceManager();
+		tx = pm.currentTransaction();
+
+		try {
+			tx.begin();
+			System.out.println("   * Storing an object: " + usuarioDAO);
+			pm.makePersistent(usuarioDAO);
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("   $ Error storing an object: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+		pm = pmf.getPersistenceManager();
+		/*
+		 * By default only 1 level is retrieved from the db so if we wish to fetch more
+		 * than one level, we must indicate it
+		 */
+		pm.getFetchPlan().setMaxFetchDepth(3);
+
+		tx = pm.currentTransaction();
+		List<User> products = new ArrayList<>();
+
+		try {
+			System.out.println("   * Retrieving an Extent for Products.");
+
+			tx.begin();
+			Extent<User> extent = pm.getExtent(User.class, true);
+
+			for (User product : extent) {
+				products.add(product);
+			}
+
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("   $ Error retrieving an extent: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+		
+
+	System.out.println(products.get(0).getSesiones().toString());
+
+	
+	
+	//UserDAO.getInstance().save(usuarioDAO);
+	//-----------------------------------------------------------------------------------------
 	//System.out.print(UserDAO.getInstance().find("Alfonso") + "\n");
 	//System.out.println(RetoDAO.getInstance().find("Correr un poco"));
 	//System.out.println(RetoDAO.getInstance().findFromUser(usuarioDAO.getNombre(), "Correr un poco"));
